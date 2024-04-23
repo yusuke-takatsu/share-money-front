db3de862dec39b9420779076378e176e9d6af2cc
github VinousTy <github ys.taka44@gmail.com>
2024-04-23 22:18:16 +0900

    feat:add create theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
+import { theme } from '@/utils';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <MUIThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </MUIThemeProvider>
+  );
 }
---
+++ src/utils/muiThemeClient.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FBB950', 
+    },
+    secondary: {
+      main: '#DD4B39'
+    }
+  },
+})
