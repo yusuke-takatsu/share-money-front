c6dccdb00060f3db40a8ccd9d73a325967f2dd59
github VinousTy <github ys.taka44@gmail.com>
2024-04-26 16:39:11 +0900

    feat:setting custom axios

---
+++ src/lib/axios/axios.ts
@@ -0,0 +1,23 @@
+import { COOKIE_KEY_NAMES } from '@/utils/constants/variable';
+import Axios, { AxiosError } from 'axios';
+
+export const axios = Axios.create({
+  xsrfCookieName: COOKIE_KEY_NAMES.XSRF_TOKEN,
+  xsrfHeaderName: COOKIE_KEY_NAMES.X_XSRF_TOKEN,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-with': 'XMLHttpRequest',
+  },
+  withCredentials: true,
+  withXSRFToken: true,
+});
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error.response?.data);
+  }
+);
