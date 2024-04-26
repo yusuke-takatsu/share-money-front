9439ce71516c87435653fba5e7f5a541588fad7d
github VinousTy <github ys.taka44@gmail.com>
2024-04-26 16:37:25 +0900

    feat:add default css parameter

--- src/utils/themeClient.ts
+++ src/utils/themeClient.ts
@@ -1,7 +1,34 @@
 const colors = {
   background: {
-    gray: '#EDEBE8'
-  }
-}
+    gray: '#EDEBE8',
+    white: '#FFF',
+  },
+};
+const fonts = {
+  size: {
+    sm: '12px',
+    md: '14px',
+    lg: '16px',
+    xl: '20px',
+    xxl: '24px',
+  },
+  weight: {
+    normal: '400',
+    bold: '700',
+  },
+};
 
-export const bgColor = colors.background
+const line = {
+  height: {
+    sm: '18px',
+    md: '21px',
+    lg: '24px',
+    xl: '30px',
+    xxl: '36px',
+  },
+};
+
+export const bgColor = colors.background;
+export const fontSize = fonts.size;
+export const fontWeight = fonts.weight;
+export const lineHeight = line.height;
