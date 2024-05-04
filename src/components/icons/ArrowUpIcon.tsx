268386ee3f52011969a23b6b45efc3c1bc6b44e8
github VinousTy <github ys.taka44@gmail.com>
2024-05-04 15:28:19 +0900

    feat:create select box icons

---
+++ src/components/icons/ArrowUpIcon.tsx
@@ -0,0 +1,19 @@
+export const ArrowUpIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M18 15L12 9L6 15"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
